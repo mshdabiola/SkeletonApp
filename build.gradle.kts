169143517ac1b5c2b4edf1ad49797e1aa177d656
mshabiola <mshdabiola@gmal.com>
2023-01-23 15:55:46 +0100

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,3 +31,28 @@ versionCatalogUpdate {
         keepUnusedPlugins.set(true)
     }
 }
+
+
+
+tasks.create("add_spot"){
+    val proj=project.rootDir
+
+  //  doFirst{
+        print("add spotless to git")
+        val gitHooksDirectory = File("$proj/.git/hooks/")
+        if (!gitHooksDirectory.exists()) gitHooksDirectory.mkdirs()
+        File("$proj/.git/hooks", "pre-commit").writeText(
+            """
+    #!/bin/bash
+    echo "Running spotless check"
+    ./gradlew spotlessApply --init-script gradle/init.gradle.kts
+"""
+        )
+
+        exec {
+            commandLine("chmod","+x", ".git/hooks/pre-commit")
+        }
+
+  //  }
+
+}
