9df554721124cd84d3d08444e4ebb00414f2c30d
mshabiola <mshdabiola@gmal.com>
2023-02-21 23:05:39 +0100

    feat: add snack to main screen

--- feature/mainscreen/src/main/java/com/mshdabiola/mainscreen/MainViewModel.kt
+++ feature/mainscreen/src/main/java/com/mshdabiola/mainscreen/MainViewModel.kt
@@ -5,9 +5,16 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mshdabiola.data.repository.ModelRepository
 import com.mshdabiola.model.Model
+import com.mshdabiola.ui.data.Notify
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.collections.immutable.toImmutableList
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
@@ -17,15 +24,46 @@ class MainViewModel
     private val modelRepository: ModelRepository,
 ) : ViewModel() {
 
-    val mainState = modelRepository
+    val modelState = modelRepository
         .getModels()
 //        .asResult()
 //        .stateIn(scope = viewModelScope,
 //            started = SharingStarted.WhileSubscribed(3000), initialValue = MainState.Loading)
 
+
+
+    private val _mainState= MutableStateFlow(MainState())
+    val mainState = _mainState.asStateFlow()
+    init {
+        viewModelScope.launch {
+            delay(2000)
+            addNotify("Add Model")
+            addNotify("remove model")
+        }
+
+    }
     fun insert(model: Model) {
         viewModelScope.launch(Dispatchers.IO) {
             modelRepository.insertModel(model)
         }
     }
+
+    private fun addNotify(text: String) {
+        Timber.d("Add")
+        val notifies = mainState.value.messages.toMutableList()
+
+        notifies.add(Notify(message = text, callback = ::onNotifyDelive))
+       _mainState.update {
+           it.copy(messages = notifies.toImmutableList())
+       }
+    }
+
+    private fun onNotifyDelive() {
+        Timber.d("Remove")
+        val notifies = mainState.value.messages.toMutableList()
+
+        notifies.removeFirst()
+        _mainState.value = mainState.value.copy(messages = notifies.toImmutableList())
+    }
+
 }
